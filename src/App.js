8106ac361d768a07fda500e96848dbe977dc4333
Ray Maldonado <rm01964@rmn.com>
2020-03-22 21:11:56 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload - change 6
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
