b5547e89f6ae72fab09079e61e10ee509fdbf8bc
Ray Maldonado <rm01964@rmn.com>
2020-03-27 12:09:58 -0500

    Changing string !

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload - change 1
+          Edit <code>src/App.js</code> and save to reload - change 2
         </p>
         <a
           className="App-link"
