919097dc64e9abb3e1a58ac7ac1ee1f7ed6830eb
Ray Maldonado <rm01964@rmn.com>
2020-03-27 12:19:37 -0500

    Change version string in main app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          Edit <code>src/App.js</code> and save to reload - change 2
+          Edit <code>src/App.js</code> and save to reload - version 1
         </p>
         <a
           className="App-link"
